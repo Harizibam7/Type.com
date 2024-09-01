ade758a97d69ce4e3c88b63e3243c5b802ea7512
Harizibam7 <harizibamv@gmail.com>
2024-09-01 22:42:26 +0530

    Side bar

--- frontend/src/Components/Home/Home_Sidebar.jsx
+++ frontend/src/Components/Home/Home_Sidebar.jsx
@@ -1,5 +1,32 @@
 export function Home_Sidebar(){
-    return <div>
-        From Home Sidebar
+    return <div className="h-full w-full bg-slate-800 text-xl flex-f ">
+        <div >   
+            <div>
+                play
+            </div>
+            <div>
+                puzzles
+            </div>
+            <div>
+                learn
+            </div>
+            <div>
+                watch
+            </div>
+            <div>
+                News
+            </div>
+            <div>
+                Social
+            </div>
+        </div>
+        <div>
+            <button>
+                sigin
+            </button>
+            <button>
+                    logout
+            </button>
+        </div>
     </div>
 }
